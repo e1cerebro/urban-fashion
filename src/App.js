a364ae6fa7d6fbdf14521b2c9299e39e828d5280
Christian Nwachukwu <nwachukwu16@gmail.com>
2020-04-10 11:14:39 -0400

    Added the collection preview and collection items components

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Homepage from './pages/homepage/Homepage';
+import Shop from './pages/shop/Shop';
 import './App.scss';
-
-const Test = props => {
-  return (
-    <div>
-      Test <button onClick={() => props.history.goBack()}>Go back</button>
-    </div>
-  );
-};
 
 const App = () => {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={Homepage} />
-        <Route exact path='/hats' component={Test} />
+        <Route exact path='/shop' component={Shop} />
       </Switch>
     </div>
   );
